f26dc1875f67b9c0876c4f2bb044e05ef7610629
JuanDaGue <dj.jd2009@hotmail.com>
2024-12-20 14:35:30 -0500

    fix

--- components/Shared/BlockSongs/BlockSongs.tsx
+++ components/Shared/BlockSongs/BlockSongs.tsx
@@ -1,5 +1,5 @@
 import { BlockSongsProps } from "./BlockSongs.types";
-import { CarouselSong } from "./CarouselSong";
+// import { CarouselSong } from "./CarouselSong";
 
 export function BlockSongs(props: BlockSongsProps) {
     const { title, Songs, isMyList } = props;
@@ -9,7 +9,8 @@ export function BlockSongs(props: BlockSongsProps) {
     return (
         <div className="-top-16 relative px-[4%] md:pt-20 md:pb-20 overflow-auto bg-[#171717]">
         <h3 className="text-2xl font-semibold mb-3">{title}</h3>
-        <CarouselSong Songs={Songs} isMyList={isMyList} />
+        {/* <CarouselSong Songs={Songs} isMyList={isMyList} /> */}
+        Carousell
         </div>
     );
 }
