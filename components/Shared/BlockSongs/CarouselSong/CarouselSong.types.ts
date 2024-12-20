9abb0f570c9c200055c111836bfe2f703fbfcacc
JuanDaGue <dj.jd2009@hotmail.com>
2024-12-20 14:24:18 -0500

    fix

--- components/Shared/BlockSongs/CarouselSong/CarouselSong.types.ts
+++ components/Shared/BlockSongs/CarouselSong/CarouselSong.types.ts
@@ -1,5 +1,4 @@
 import { SongData } from "@/app/(home)/components/TrendingLyrics/TrendingLyrics.types";
-import { Song } from "@prisma/client";
 export type CarouselSongProps = {
     Songs: SongData[];
     isMyList: boolean;
