935fc57cf3d44ab80c6d0712e2a70e2f228950d3
JuanDaGue <dj.jd2009@hotmail.com>
2024-12-20 13:40:29 -0500

    try

--- app/api/uploadLyrics.ts
+++ app/api/uploadLyrics.ts
@@ -6,25 +6,30 @@ const prisma = new PrismaClient();
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         const { title, artist, album, genre, releaseDate, duration, lyrics, mainIdea, description } = req.body;
-        
+
         try {
-        const newSong = await prisma.song.create({
-            data: {
-            title,
-            artist,
-            album,
-            genre,
-            releaseDate: new Date(releaseDate),
-            duration,
-            lyrics,
-            mainIdea,
-            description,
-            },
-        });
-        res.status(200).json(newSong);
-        } catch (error: any) {  // Añadido : any para manejar el tipo
-        console.error('Error creating song:', error);  // Usar la variable `error`
-        res.status(500).json({ error: 'Error creating song' });
+            const newSong = await prisma.song.create({
+                data: {
+                    title,
+                    artist,
+                    album,
+                    genre,
+                    releaseDate: new Date(releaseDate),
+                    duration,
+                    lyrics,
+                    mainIdea,
+                    description,
+                },
+            });
+            res.status(200).json(newSong);
+        } catch (error: unknown) {  // Usar `unknown` en lugar de `any`
+            if (error instanceof Error) {  // Verificar si `error` es una instancia de `Error`
+                console.error('Error creating song:', error.message);
+                res.status(500).json({ error: error.message });
+            } else {
+                console.error('Unexpected error', error);
+                res.status(500).json({ error: 'An unexpected error occurred' });
+            }
         }
     } else {
         res.status(405).json({ error: 'Method not allowed' });
